2803a325c3d2a3304d510868f602ad06c0f01d3c
Brittany Klose <brittany.klose@yahoo.com>
2025-03-13 14:29:03 -0400

    sql update and csv files

--- PatientsTable.sql
+++ PatientsTable.sql
@@ -1,34 +1,44 @@
+-- Initialize Database
+
+create database dentistsdb;
+use dentistsdb;
+
 -- Create Patients Table
 CREATE TABLE patients (
     patient_id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
+    phone  varchar(200),
+    email varchar(50) not null,
     date_of_birth DATE NOT NULL,
-    age VARCHAR(45) NOT NULL,
-    gender ENUM('Male', 'Female', 'Other') NOT NULL,
-    contact_number VARCHAR(20),
-    address VARCHAR(255),
-    name var,
+    address varchar(100) not null,
+    city varchar(100),
+    state  varchar(100),
+    zipcode  varchar(100),
+    gender  varchar(100),
+
+    constraint gender_constraint check (gender in
+    ('F', 'M', 'O'))
 );
 
 -- Create Procedures Table
 CREATE TABLE procedures (
     procedure_id INT PRIMARY KEY AUTO_INCREMENT,
-    procedure_name VARCHAR(100) NOT NULL,
-    cost DECIMAL(10,2) NOT NULL,
-    medical_practitioner VARCHAR(45)
+    procedure_name VARCHAR(100) NOT NULL
 );
 
 -- Create Patient History Table
 CREATE TABLE patient_history (
-    record_id INT NOT NULL,
+    record_id INT primary key,
     patient_id INT NOT NULL,
     procedure_id INT NOT NULL,
     procedure_date DATE NOT NULL,
-    dentist VARCHAR(100) NOT NULL
+    dentist VARCHAR(100) NOT NULL,
     assistant VARCHAR(100) NOT NULL,
-    FOREIGN KEY (patient_id) REFERENCES patients(patient_id) ON DELETE CASCADE,
-    FOREIGN KEY (procedure_id) REFERENCES procedures(procedure_id) ON DELETE CASCADE
+    notes  varchar(200),
+
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id), 
+    FOREIGN KEY (procedure_id) REFERENCES procedures(procedure_id), 
     
     constraint dentist_constraint check(
     dentist in
@@ -36,5 +46,11 @@ CREATE TABLE patient_history (
     
     constraint assistant check(
     assistant in
-    ('Sam Winchester','Nathaniel bacon','Nicolas Jackson','Mark Grayson', 'Nolan Edward', 'Harry Porter')),
+    ('Sam Winchester','Nathaniel bacon','Nicolas Jackson','Mark Grayson', 'Nolan Edward', 'Harry Porter'))
 );
+
+select * from patients;
+
+select * from patient_history;
+
+select * from procedures
