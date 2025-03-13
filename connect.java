487d51e40674d94cf5c8b6216ada90467c81511f
Brittany Klose <brittany.klose@yahoo.com>
2025-03-13 13:27:28 -0400

    test

--- connect.java
+++ connect.java
@@ -2,6 +2,7 @@ package project;
 import java.sql.Connection;
 import java.sql.DriverManager;
 //import java.sql.SQLException;
+// adding something
 public class connect
 {
 	public static void main(String[] args)
